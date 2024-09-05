c04ed09e5fb7ad80886cb5a88dc1846bebf7cf60
Holden Mitchell <holdenmitchell@users.noreply.github.com>
2024-09-05 15:50:09 -0500

    Fix party type

--- spec/parsers/parties_spec.rb
+++ spec/parsers/parties_spec.rb
@@ -4,13 +4,8 @@ RSpec.describe OscnScraper::Parsers::Parties do
     parsed_html = load_and_parse_fixture(fixture_path)
     data = described_class.parse(parsed_html)
 
-    expect(data[:parties].count).to eq 3
-    expect(data[:parties]).to include(
-      {
-        name: 'PENA,  ANTHONY  RAMIRO',
-        link: 'GetPartyRecord.aspx?db=oklahoma&cn=CF-2021-1511&id=18498184',
-        party_type: 'Defendant'
-      }
-    )
+    expect(data[:parties].count).to eq 2
+    expect(data[:parties].first[:link]).to eq 'GetPartyRecord.aspx?db=oklahoma&cn=CF-2024-3682&id=19656360'
+    expect(data[:parties].first[:party_type]).to eq 'Defendant'
   end
 end
